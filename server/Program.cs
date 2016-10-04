33e9d18e70d7425b8ccc539a7709652cfd55b82f
pamparo <pedroamparo4@gmail.com>
2016-10-04 23:33:18 +0200

    Initial commit

---
+++ server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? port = null;
+            string path = string.Empty;
+            int default_port = 80;
+            string default_path = Directory.GetCurrentDirectory();
+
+            if (args.GetLength(0) > 0)
+            {
+                port = Convert.ToInt16(args[0]);
+            }
+            if (args.GetLength(0) > 1)
+            {
+                path = args[1];
+            }
+
+            if (port == null)
+            {
+                port = default_port;
+            }
+
+            if (string.IsNullOrEmpty(path))
+            {
+                path = default_path;
+            }
+
+            Console.WriteLine("PORT: " + port);
+            Console.WriteLine("PATH: " + path);
+            Console.ReadLine();
+        }
+    }
+}
