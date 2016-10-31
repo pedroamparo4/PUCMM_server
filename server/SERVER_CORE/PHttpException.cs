d63d85d4d0a47a0bf2614cd96d489eb384974a30
pamparo <pedroamparo4@gmail.com>
2016-10-31 14:46:54 +0100

    New Files Added

---
+++ server/SERVER_CORE/PHttpException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace server.SERVER_CORE
+{
+    [Serializable]
+    public class PHttpException : Exception
+    {
+        public PHttpException()
+        {
+        }
+
+        public PHttpException(string message)
+            : base(message)
+        {
+        }
+
+        public PHttpException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected PHttpException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
